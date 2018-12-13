28469d6a5a9468217979b74c2413b6f554fc7fe5
kgc <kgc1753@naver.com>
2018-12-13 21:43:42 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "indent": ["warn", 4],
+        "semi": 2,
+        "no-unused-vars": 1,
+        "no-console": "off"
+    }
+};
