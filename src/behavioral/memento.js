934d5c9c05a614363560278ef38926e9b996c72f
kgc <kgc1753@naver.com>
2018-12-13 21:53:04 +0900

    memento

---
+++ src/behavioral/memento.js
@@ -0,0 +1,63 @@
+import utils from '../utils/index';
+const {log: {e}, checker:{is}} = utils;
+
+/**Assuming
+ * Task란 할일이 있고 그 하위에도 여러 Task들이 중첩되어 있다.
+ * studying
+ *      -cs
+ *          -network
+ *          -algorithm
+ *              -dfs
+ *              -bfs
+ *      -english
+ */
+const Task = class extends Set {
+    constructor(title = '') {
+        super();
+        this._title = title;
+    }
+    add(task) {
+        if(!is(task, Task)) e('Invalid Type!');
+        super.add(task);
+        return this;
+    } 
+    getTask(task) {
+        return this.has(task) ? [...this].filter(t => t===task)[0] : e(`"${task._title}" is not existed`);
+    }
+    static reStore(tasks) {
+        tasks = typeof tasks === 'string' ? JSON.parse(tasks) : tasks;
+        const task = new Task(tasks.title);
+        tasks.list.map(t=>task.add(Task.reStore(t)));
+        return task;
+    }
+    toJSON() {
+        return {
+            title: this._title,
+            list: [...this]
+        };
+    }
+    clear(){} delete(){}
+};
+
+
+//Setting
+const studyingTask = new Task('studying');
+const csTask = new Task('cs');
+studyingTask.add(csTask).add(new Task('english'));
+const algoTask = new Task('algorithm');
+csTask.add(new Task('network')).add(algoTask);
+algoTask.add(new Task('dfs')).add(new Task('bfs'));
+
+//Usage
+const store = JSON.stringify(studyingTask); 
+console.log(store); //{"_title":"studying","list":[{"_title":"cs","list":[{"_title":"network","list":[]},{"_title":"algorithm","list":[{"_title":"dfs","list":[]},{"_title":"bfs","list":[]}]}]},{"_title":"english","list":[]}]}
+console.log(Task.reStore(store));
+
+/** Explaination
+ * Task가 Set를 상속받은 이유.
+ *    Task가 하위에 list를 가지고 있는 객체이기 보다는 할일들의 리스트 그 자체이기 때문.
+ * 
+ * memento 패턴은 자신의 상태값을 저장하고 복원하는 방식을 일컫는다.
+ * 이 패턴은 대게 혼자 쓰이지 않고 command패턴과 같이 쓰인다. command패턴에서 각 command를 execute한후 undo할때 쓰이기 좋다.
+ * reStore와 같은 재귀적인 처리를 composit 패턴으로 볼 수 있다.
+*/
