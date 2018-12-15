eac78720f93cc21b95954b6801fe54104ec9b786
kgc <kgc1753@naver.com>
2018-12-15 21:20:31 +0900

    addes staticIs

--- src/utils/checker.js
+++ src/utils/checker.js
@@ -1,5 +1,12 @@
 const is = (child, parent) => child instanceof parent;
-
+const staticIs = (child, parent) => {
+    let a = child;
+    do{
+        if(a === parent) break;
+    } while((a = a.__proto__));
+    return a === parent;
+};
 export default {
-    is
+    is,
+    staticIs
 };
