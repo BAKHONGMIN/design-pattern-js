17f2c4344249fe5df2290e35b8a0238f2fe1f536
kgc <kgc1753@naver.com>
2018-12-17 01:16:05 +0900

    added custom log

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,5 +1,7 @@
-const error = (message) => {throw message};
+const error = (m) => {throw m;};
+const log = (m, color) => !color ? console.log(m) : console.log('%c'+m,`color:${color};font-weight:bold`);
 
 export default {
-    e: error
+    e: error,
+    log
 };
